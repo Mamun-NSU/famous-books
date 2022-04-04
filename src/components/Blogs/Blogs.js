12295978d43f9457027035edf2439998c000c424
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-04-04 23:42:25 +0600

    blogs layout created

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,7 +4,21 @@ import "./Blogs.css";
 const Blogs = () => {
   return (
     <div>
-      <h1>This is Blogs Page.</h1>
+      <section class="blog-container">
+        <hr />
+        <div class="single-blog">
+          <h5>WHAT IS CONTEXT API?</h5>
+          <p><span class="answare-keyword">Answare:</span> it </p>
+        </div>
+        <div class="single-blog">
+          <h5>WHAT IS SEMANTIC TAGS IN HTML?</h5>
+          <p><span class="answare-keyword">Answare:</span>We  </p>
+        </div>
+        <div class="single-blog">
+          <h5>WHAT IS THE DIFFERENCE BETWEEN INLINE AND BLOCK ELEMENTS?</h5>
+          <p><span class="answare-keyword">Answare:</span> Local  </p>
+        </div>
+      </section>
     </div>
   );
 };
