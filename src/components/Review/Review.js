3efd3e98a12859f9a3c3850c20adf7000477997e
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-04-05 20:13:38 +0600

    readme.md file and other section update successfully

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./Review.css";
 
 const Review = (props) => {
-  const { user_name, user_image, review, rating } = props.review;
+  const { user_name, user_image, review, book, rating } = props.review;
   return (
     <div className="review">
       <p>
         <img src={user_image} alt="user image"></img> <span className="user_name">{user_name}</span>
       </p>
+      <p>Review on: <span className="book">{book}</span></p>
       <p>{review}</p>
-      <p>Rating: {rating}</p>
+      <p> <span className="rating">Rating:</span> {rating}</p>
     </div>
   );
 };
